a43757d499df25d9340d64f121f058e0141bb03d
peinlcy <peinlcy@gmail.com>
2024-12-02 22:14:30 +0800

    add borsh serialize

--- src/fixed_vector.rs
+++ src/fixed_vector.rs
@@ -1,5 +1,6 @@
 use crate::tree_hash::vec_tree_hash_root;
 use crate::Error;
+use borsh::{BorshDeserialize, BorshSerialize};
 use derivative::Derivative;
 use serde_derive::{Deserialize, Serialize};
 use std::marker::PhantomData;
@@ -45,7 +46,7 @@ pub use typenum;
 /// let long: FixedVector<_, typenum::U5> = FixedVector::from(base);
 /// assert_eq!(&long[..], &[1, 2, 3, 4, 0]);
 /// ```
-#[derive(Debug, Clone, Serialize, Deserialize, Derivative)]
+#[derive(Debug, Clone, Serialize, Deserialize, Derivative, BorshSerialize, BorshDeserialize)]
 #[derivative(PartialEq, Eq, Hash(bound = "T: std::hash::Hash"))]
 #[serde(transparent)]
 pub struct FixedVector<T, N> {
